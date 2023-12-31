7be870a053997ab379af5a89db00444146e828d8
Koter Michal <s24569@pjwstk.edu.pl>
2023-12-31 11:39:04 +0100

    feature: open settings window after click btn

--- desktop_app/ui_mainWindow.py
+++ desktop_app/ui_mainWindow.py
@@ -1,17 +1,12 @@
-from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
-    QMetaObject, QObject, QPoint, QRect,
-    QSize, QTime, QUrl, Qt)
-from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
-    QFont, QFontDatabase, QGradient, QIcon,
-    QImage, QKeySequence, QLinearGradient, QPainter,
-    QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QApplication, QFrame, QGraphicsView, QHBoxLayout,
-    QLabel, QListView, QPushButton, QSizePolicy,
-    QVBoxLayout, QWidget)
+                               QLabel, QListView, QPushButton, QSizePolicy,
+                               QVBoxLayout, QWidget, QMainWindow)
 import numpy as np
 import cv2
 from PySide6.QtGui import *
 from PySide6.QtCore import *
+
+from desktop_app.ui_settings import Ui_settings
 
 class VideoThread(QThread):
     change_pixmap_signal = Signal(np.ndarray)
@@ -23,113 +18,68 @@ class VideoThread(QThread):
             if ret:
                 self.change_pixmap_signal.emit(cv_imag)
 
+class Ui_main_frame(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi()
+        self.settings_window = None
 
-class Ui_mainFrame(object):
-    def setupUi(self, mainFrame):
-        if not mainFrame.objectName():
-            mainFrame.setObjectName(u"mainFrame")
-        mainFrame.resize(738, 539)
-        # self.webcamView = QGraphicsView(mainFrame)
-        # self.webcamView.setObjectName(u"webcamView")
-        # self.webcamView.setGeometry(QRect(30, 30, 431, 361))
-        # set webcam view
-        self.image_label = QLabel(mainFrame)
+    def setupUi(self):
+        if not self.objectName():
+            self.setObjectName(u"main_frame")
+        self.setWindowModality(Qt.ApplicationModal)
+        self.resize(761, 461)
+        self.setSizeIncrement(QSize(1, 0))
+
+        self.image_label = QLabel(self)
         self.text_label = QLabel('Webcam')
         vbox = QVBoxLayout()
         vbox.addWidget(self.image_label)
         vbox.addWidget(self.text_label)
         self.vbox = vbox
-        self.vbox.setGeometry(QRect(30, 30, 400, 350))
+        self.vbox.setGeometry(QRect(30, 30, 480, 270))
 
         self.thread = VideoThread()
         self.thread.change_pixmap_signal.connect(self.update_image)
         self.thread.start()
-        #
+        self.settings = QPushButton(self)
 
-        self.settings = QPushButton(mainFrame)
+        self.settings.clicked.connect(self.display_settings) ###
+
         self.settings.setObjectName(u"settings")
-        self.settings.setGeometry(QRect(20, 500, 80, 24))
-        self.verticalLayoutWidget = QWidget(mainFrame)
-        self.verticalLayoutWidget.setObjectName(u"verticalLayoutWidget")
-        self.verticalLayoutWidget.setGeometry(QRect(510, 30, 141, 111))
-        self.verticalLayout = QVBoxLayout(self.verticalLayoutWidget)
-        self.verticalLayout.setObjectName(u"verticalLayout")
-        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
-        self.label = QLabel(self.verticalLayoutWidget)
-        self.label.setObjectName(u"label")
-
-        self.verticalLayout.addWidget(self.label)
-
-        self.webcam = QPushButton(self.verticalLayoutWidget)
-        self.webcam.setObjectName(u"webcam")
-        self.webcam.setEnabled(True)
-        self.webcam.setChecked(False)
-
-        self.verticalLayout.addWidget(self.webcam)
-
-        self.phonecam = QPushButton(self.verticalLayoutWidget)
-        self.phonecam.setObjectName(u"phonecam")
-
-        self.verticalLayout.addWidget(self.phonecam)
-
-        self.horizontalLayoutWidget = QWidget(mainFrame)
-        self.horizontalLayoutWidget.setObjectName(u"horizontalLayoutWidget")
-        self.horizontalLayoutWidget.setGeometry(QRect(130, 420, 221, 71))
-        self.horizontalLayout = QHBoxLayout(self.horizontalLayoutWidget)
-        self.horizontalLayout.setObjectName(u"horizontalLayout")
-        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
-        self.start = QPushButton(self.horizontalLayoutWidget)
+        self.settings.setGeometry(QRect(20, 420, 80, 24))
+        self.list_view = QListView(self)
+        self.list_view.setObjectName(u"list_view")
+        self.list_view.setGeometry(QRect(550, 50, 180, 250))
+        self.acions_log = QLabel(self)
+        self.acions_log.setObjectName(u"acions_log")
+        self.acions_log.setEnabled(True)
+        self.acions_log.setGeometry(QRect(550, 30, 181, 21))
+        self.start = QPushButton(self)
         self.start.setObjectName(u"start")
-
-        self.horizontalLayout.addWidget(self.start)
-
-        self.stop = QPushButton(self.horizontalLayoutWidget)
+        self.start.setGeometry(QRect(250, 370, 107, 24))
+        self.stop = QPushButton(self)
         self.stop.setObjectName(u"stop")
-        self.stop.setCursor(QCursor(Qt.ArrowCursor))
-        self.stop.setMouseTracking(False)
-        self.stop.setAcceptDrops(False)
+        self.stop.setGeometry(QRect(370, 370, 101, 24))
 
-        self.horizontalLayout.addWidget(self.stop)
+        # self.stop.clicked.connect(self.thread.terminate)
 
-        self.verticalLayoutWidget_2 = QWidget(mainFrame)
-        self.verticalLayoutWidget_2.setObjectName(u"verticalLayoutWidget_2")
-        self.verticalLayoutWidget_2.setGeometry(QRect(510, 190, 181, 311))
-        self.verticalLayout_2 = QVBoxLayout(self.verticalLayoutWidget_2)
-        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
-        self.verticalLayout_2.setContentsMargins(0, 0, 0, 0)
-        self.label_2 = QLabel(self.verticalLayoutWidget_2)
-        self.label_2.setObjectName(u"label_2")
-        self.label_2.setEnabled(True)
+        self.processing_label = QLabel(self)
+        self.processing_label.setObjectName(u"processing_label")
+        self.processing_label.setGeometry(QRect(280, 340, 171, 16))
 
-        self.verticalLayout_2.addWidget(self.label_2)
+        self.retranslateUi(self)
 
-        self.line = QFrame(self.verticalLayoutWidget_2)
-        self.line.setObjectName(u"line")
-        self.line.setFrameShape(QFrame.HLine)
-        self.line.setFrameShadow(QFrame.Sunken)
-
-        self.verticalLayout_2.addWidget(self.line)
-
-        self.listView = QListView(self.verticalLayoutWidget_2)
-        self.listView.setObjectName(u"listView")
-
-        self.verticalLayout_2.addWidget(self.listView)
-
-
-        self.retranslateUi(mainFrame)
-
-        QMetaObject.connectSlotsByName(mainFrame)
+        QMetaObject.connectSlotsByName(self)
     # setupUi
 
-    def retranslateUi(self, mainFrame):
-        mainFrame.setWindowTitle(QCoreApplication.translate("mainFrame", u"Widget", None))
-        self.settings.setText(QCoreApplication.translate("mainFrame", u"Settings", None))
-        self.label.setText(QCoreApplication.translate("mainFrame", u"Select video source", None))
-        self.webcam.setText(QCoreApplication.translate("mainFrame", u"Webcam", None))
-        self.phonecam.setText(QCoreApplication.translate("mainFrame", u"Phone camera", None))
-        self.start.setText(QCoreApplication.translate("mainFrame", u"Start", None))
-        self.stop.setText(QCoreApplication.translate("mainFrame", u"Stop", None))
-        self.label_2.setText(QCoreApplication.translate("mainFrame", u"Logs", None))
+    def retranslateUi(self, main_frame):
+        main_frame.setWindowTitle(QCoreApplication.translate("main_frame", u"Widget", None))
+        self.settings.setText(QCoreApplication.translate("main_frame", u"Settings", None))
+        self.acions_log.setText(QCoreApplication.translate("main_frame", u"Actions Log", None))
+        self.start.setText(QCoreApplication.translate("main_frame", u"Start", None))
+        self.stop.setText(QCoreApplication.translate("main_frame", u"Stop", None))
+        self.processing_label.setText(QCoreApplication.translate("main_frame", u"Start/Stope gesture processing", None))
     # retranslateUi
 
     @Slot(np.ndarray)
@@ -142,9 +92,19 @@ class Ui_mainFrame(object):
         h, w, c = rgb_image.shape
         bytes_per_line = c * w
         convert_to_Qt_format = QImage(rgb_image.data, w, h, bytes_per_line, QImage.Format_RGB888)
-        p = convert_to_Qt_format.scaled(400, 350)
+        p = convert_to_Qt_format.scaled(480, 270)
         return QPixmap.fromImage(p)
 
     def closeEvent(self, event):
         self.thread.stop()
         event.accept()
+
+    def display_settings(self):
+        if self.settings_window is None:
+            self.settings_window = Ui_settings(self)
+            self.settings_window.show()
+        else:
+            self.settings_window.close()
+            self.settings_window = None
+        # self.hide()
+
--- desktop_app/widget.py
+++ desktop_app/widget.py
@@ -1,18 +1,12 @@
 import sys
 
-from PySide6.QtWidgets import QApplication, QWidget, QMainWindow
-from ui_mainWindow import Ui_mainFrame
+from PySide6.QtWidgets import QApplication
 
-
-class Widget(QMainWindow):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.ui = Ui_mainFrame()
-        self.ui.setupUi(self)
+from ui_mainWindow import Ui_main_frame
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    widget = Widget()
+    widget = Ui_main_frame()
     widget.show()
     sys.exit(app.exec())
