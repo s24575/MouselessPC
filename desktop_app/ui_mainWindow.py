1486322ea9b363307b031444d4952309e7c16fe4
Koter Michal <s24569@pjwstk.edu.pl>
2024-01-08 08:10:50 +0100

    Add camera not found display

--- desktop_app/ui_mainWindow.py
+++ desktop_app/ui_mainWindow.py
@@ -1,12 +1,13 @@
 from PySide6.QtWidgets import (QApplication, QFrame, QGraphicsView, QHBoxLayout,
                                QLabel, QListView, QPushButton, QSizePolicy,
-                               QVBoxLayout, QWidget, QMainWindow)
+                               QVBoxLayout, QWidget, QMainWindow, QListWidget)
 import numpy as np
 import cv2
 from PySide6.QtGui import *
 from PySide6.QtCore import *
 
 from desktop_app.ui_settings import Ui_settings
+
 
 class VideoThread(QThread):
     change_pixmap_signal = Signal(np.ndarray)
@@ -17,6 +18,7 @@ class VideoThread(QThread):
             ret, cv_imag = cap.read()
             if ret:
                 self.change_pixmap_signal.emit(cv_imag)
+
 
 class Ui_main_frame(QMainWindow):
     def __init__(self):
@@ -32,25 +34,31 @@ class Ui_main_frame(QMainWindow):
         self.setSizeIncrement(QSize(1, 0))
 
         self.image_label = QLabel(self)
+        qpixmap = QPixmap("camera_not_found.png")
+        qpixmap = qpixmap.scaledToWidth(480)
+        qpixmap = qpixmap.scaledToHeight(270)
+        self.image_label.setPixmap(qpixmap)
         self.text_label = QLabel('Webcam')
         vbox = QVBoxLayout()
         vbox.addWidget(self.image_label)
         vbox.addWidget(self.text_label)
+
         self.vbox = vbox
         self.vbox.setGeometry(QRect(30, 30, 480, 270))
 
-        self.thread = VideoThread()
-        self.thread.change_pixmap_signal.connect(self.update_image)
-        self.thread.start()
-        self.settings = QPushButton(self)
+        self.video_thread = VideoThread()
+        self.video_thread.change_pixmap_signal.connect(self.update_image)
+        self.video_thread.start()
 
-        self.settings.clicked.connect(self.display_settings) ###
+        self.settings = QPushButton(self)
+        self.settings.clicked.connect(self.display_settings)
 
         self.settings.setObjectName(u"settings")
         self.settings.setGeometry(QRect(20, 420, 80, 24))
-        self.list_view = QListView(self)
+        self.list_view = QListWidget(self)
         self.list_view.setObjectName(u"list_view")
         self.list_view.setGeometry(QRect(550, 50, 180, 250))
+
         self.acions_log = QLabel(self)
         self.acions_log.setObjectName(u"acions_log")
         self.acions_log.setEnabled(True)
@@ -61,8 +69,6 @@ class Ui_main_frame(QMainWindow):
         self.stop = QPushButton(self)
         self.stop.setObjectName(u"stop")
         self.stop.setGeometry(QRect(370, 370, 101, 24))
-
-        # self.stop.clicked.connect(self.thread.terminate)
 
         self.processing_label = QLabel(self)
         self.processing_label.setObjectName(u"processing_label")
@@ -96,15 +102,12 @@ class Ui_main_frame(QMainWindow):
         return QPixmap.fromImage(p)
 
     def closeEvent(self, event):
-        self.thread.stop()
+        self.video_thread.terminate()
+        self.log_thread.terminate()
         event.accept()
 
     def display_settings(self):
         if self.settings_window is None:
             self.settings_window = Ui_settings(self)
-            self.settings_window.show()
-        else:
-            self.settings_window.close()
-            self.settings_window = None
-        # self.hide()
-
+        self.settings_window.show()
+        self.hide()
