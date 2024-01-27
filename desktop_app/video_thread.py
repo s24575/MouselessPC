10d30b6535955d89ecaba9a4ed9575614202549a
s24575 <91730952+s24575@users.noreply.github.com>
2024-01-27 21:10:40 +0100

    Allow using a mouse around the edges of the screen(#19)

--- desktop_app/video_thread.py
+++ desktop_app/video_thread.py
@@ -1,15 +1,18 @@
+from typing import Tuple
+
 import cv2
 import numpy as np
 from PySide6.QtCore import QThread, Signal
 import time
 
 from model.model import Model
+from utils.consts import Consts
 from utils.hand_gesture_image_collector import HandGestureImageCollector
 from utils.mouse_controller import MouseController
 
 
 class VideoThread(QThread):
-    change_pixmap_signal = Signal(np.ndarray, object, object)
+    change_pixmap_signal = Signal(np.ndarray)
     image_collector = HandGestureImageCollector()
     gesture_signal = Signal(str)
     process_gestures = False
@@ -28,22 +31,22 @@ class VideoThread(QThread):
                 time.sleep(1)
                 continue
             image = cv2.flip(image, 1)
-            self.change_pixmap_signal.emit(image, None, None)
-            hand_landmarks, main_landmark_position = self.image_collector.get_landmark_positions(image)
-            if self.process_gestures and hand_landmarks is not None:
-                gesture = self.activate(hand_landmarks, main_landmark_position)
-                self.gesture_signal.emit(gesture)
+            self.change_pixmap_signal.emit(image)
+            if self.process_gestures:
+                hand_landmarks, main_landmark_position = self.image_collector.get_landmark_positions(image)
+                if hand_landmarks is not None:
+                    gesture = self.activate(hand_landmarks, main_landmark_position)
+                    self.gesture_signal.emit(gesture)
 
-    def activate(self, hand_landmarks, main_landmark_position):
+    def activate(self, hand_landmarks, normalized_position):
         hand_gesture, chance = self.model.predict(hand_landmarks)
-        print(hand_gesture, chance)
-        x_norm, y_norm = main_landmark_position.x, main_landmark_position.y
-        x, y = x_norm * self.screen_width, y_norm * self.screen_height
+        x, y = MouseController.calculate_mouse_position(normalized_position.x, normalized_position.y,
+                                                        self.screen_width, self.screen_height)
         MouseController.move_to(x, y)
 
-        if self.previous_gesture_name != hand_gesture.name and chance > 0.80:
+        minimum_certainty = 0.80
+        if self.previous_gesture_name != hand_gesture.name and chance > minimum_certainty:
             MouseController.execute_mouse_action(hand_gesture.action)
-
             self.previous_gesture_name = hand_gesture.name
 
         return hand_gesture.name
--- utils/consts.py
+++ utils/consts.py
@@ -1,5 +1,6 @@
 class Consts:
     HAND_IMG_SIZE: int = 256
+    HAND_PADDING: int = 15
+    SCREEN_MARGIN: int = 200
 
     LANDMARKS_PATH = 'model/gesture_classifier/landmarks.csv'
-
